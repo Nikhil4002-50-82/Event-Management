46ebff16cb308fea6a0761359649c2096841841b
Nikhil <nikhil.4002.50.82@gmail.com>
2025-07-15 14:37:21 +0530

    setup .env and .gitignore files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import cors from "cors";
+import {Pool} from "pg";
 
 const app=express();
 const port=5000;
@@ -10,6 +11,14 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+
+const db=new Pool({
+    host:process.env.host,
+    user:process.env.user,
+    port:"5432",
+    database:process.env.database,
+    password:process.env.password,
+});
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}.`);
