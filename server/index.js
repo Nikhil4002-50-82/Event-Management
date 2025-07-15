b6ad84a99a33fd815cb4deb856793b66fcb32fb4
Nikhil <nikhil.4002.50.82@gmail.com>
2025-07-15 14:09:37 +0530

    created a server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app=express();
+const port=5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}.`);
+});
